632c04e870d771af121eb685a3fd6c1fe4ea1235
zinksterz <zehngameplay@gmail.com>
2024-10-17 14:15:30 -0500

    First commit with basic navbar set up. Will continue to make navbar responsive and add a view size response menu via js

---
+++ clover_api.js
@@ -0,0 +1,18 @@
+const appSecret = "a13ba782-5191-f192-ff02-15ddebca7a60";
+const appId = "9KEFGPWYMY6EJ";
+//apiToken from test merchants Puff Me settings 
+const apiToken = "87ae1066-b047-3c36-9b29-b63e443e0767";
+//mId from test merchants business settings
+const merchantId = "RCTSTAVI0010002";
+//mIdProd is from the sandbox side where it showed a different Puff me mId
+const merchantIdProd = "TVBMESF333YP1";
+//It could or could not be this one...
+const sandboxBaseUrl = "https://apisandbox.dev.clover.com";
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${apiToken}`,
+  },
+};
+
+axios.get();
