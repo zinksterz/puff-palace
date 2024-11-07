2d0775c9d2f397589669e6bc1d27f3d172da7f39
zinksterz <zehngameplay@gmail.com>
2024-11-07 14:43:40 -0600

    Correctly stored secrets and api tokens as environment variables. Created gitignore file to make sure nothing unwanted is pushed to or visible to the public. Created relevant variables in clover_api.js and stored variables in .env file.

--- clover_api.js
+++ clover_api.js
@@ -1,22 +1,17 @@
-//Information for Puff Palace test Merchant
-const appSecret = "a13ba782-5191-f192-ff02-15ddebca7a60";
-const appId = "9KEFGPWYMY6EJ";
-//apiToken from test merchants Puff Me settings
-const apiToken = "2f0bf1a7-8ef0-d715-76ac-555c98e8cce3";
-//Ecommerce Tokens - set to require captcha
-const publicToken = "42eadf26c5f234924ad70528bda7a051";
-const privateToken = "10ea8b76-bcd9-2094-f619-5285eaafcde2";
-//MID for Merchant Account
-const MID = "RCTSTAVI0010002";
-//mIdPuffPalace is unique to the store location
-const mIdPuffPalace = "P5F27RSSX18F1";
-//It could or could not be this one...
-const sandboxBaseUrl = "https://apisandbox.dev.clover.com";
-//ex req: https://apisandbox.dev.clover.com/v3/merchants/{mId}
+require("dotenv").config();
+
+const appSecret = process.env.APP_SECRET;
+const appID = process.env.APP_ID;
+const cloverApiToken = process.env.CLOVER_API_TOKEN;
+const ePubToken = process.env.ECOM_PUB_TOKEN;
+const ePrivToken = process.env.ECOM_PRIV_TOKEN;
+const merhantOwner = process.env.MID_MERCHANT;
+const merchantStore = process.env.MID_PUFF_PALACE;
+const sandboxUrl = process.env.SANDBOX_URL;
 
 const config = {
   headers: {
-    Authorization: `Bearer ${apiToken}`,
+    Authorization: `Bearer ${cloverApiToken}`,
   },
 };
 
