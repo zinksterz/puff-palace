516f7cf15750ddee3d0c2926d34f978f0bb5bf5d
zinksterz <zehngameplay@gmail.com>
2024-11-07 15:05:24 -0600

    Need to run - leaving off on calling getMerchant() once verified and merchant is returned I will work on getting the data connect to categories.

--- clover_api.js
+++ clover_api.js
@@ -9,23 +9,52 @@ const merhantOwner = process.env.MID_MERCHANT;
 const merchantStore = process.env.MID_PUFF_PALACE;
 const sandboxUrl = process.env.SANDBOX_URL;
 
-const config = {
-  headers: {
-    Authorization: `Bearer ${cloverApiToken}`,
-  },
-};
+// const config = {
+//   headers: {
+//     Authorization: `Bearer ${cloverApiToken}`,
+//   },
+// };
 
-// axios.get();
+// // axios.get();
 
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    authorization: "Bearer 2f0bf1a7-8ef0-d715-76ac-555c98e8cce3",
-  },
-};
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     authorization: "Bearer " + cloverApiToken,
+//   },
+// };
 
-fetch("https://sandbox.dev.clover.com/v3/merchants/mId", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+// fetch("https://sandbox.dev.clover.com/v3/merchants/mId", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
+
+async function getMerchant() {
+  const merchantId = merchantStore;
+  const token = cloverApiToken;
+
+  try {
+    const response = await fetch(sandboxUrl + "/merchants" + "/" + merchantId, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer ${token}",
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error: ${response.status} - ${response.statusText");
+    }
+
+    const data = await response.json();
+    console.log("Merchant Data:", data);
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch merchant data:", error);
+  }
+}
+//node js is not going to work like this because document is only available in browser environment -- could set a button to call function or place function in html and would work
+document.addEventListener("DOMContentLoaded", () => {
+  getMerchant();
+});
