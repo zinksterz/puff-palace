ffd91a0c5b3c17be47bac24f92f1cceea2e0a257
zinksterz <zehngameplay@gmail.com>
2024-11-07 12:29:09 -0600

    Added items container to html and created horizontally scrolling container allowing category items to be displayed in an easy and readable format. Added svgs to appear for user friendly scrolling. Will go on to style a bit more and add functionality for chevrons to only appear when it makes sense: ie) If you cant scroll left dont show left chevron. Added js to detect scroll-buttons and scroll containers accordingly.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const scrollButtons = document.querySelectorAll(".scroll-button");
+
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
@@ -7,3 +9,14 @@ function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+
+scrollButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const container = button.parentElement.querySelector(".item-list");
+    const scrollAmount = 200; // Adjust the amount to scroll
+    container.scrollBy({
+      left: button.classList.contains("left") ? -scrollAmount : scrollAmount,
+      behavior: "smooth",
+    });
+  });
+});
