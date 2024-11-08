a01221ff55068b4a6e04ba9f6779f04edb582d18
zinksterz <zehngameplay@gmail.com>
2024-11-08 14:34:55 -0600

    The server is running successfully and the api is being accessed.

--- clover_api.js
+++ clover_api.js
@@ -1,36 +1,5 @@
 require("dotenv").config();
 const axios = require("axios");
-// const appSecret = process.env.APP_SECRET;
-// const appID = process.env.APP_ID;
-// const cloverApiToken = process.env.CLOVER_API_TOKEN;
-// const ePubToken = process.env.ECOM_PUB_TOKEN;
-// const ePrivToken = process.env.ECOM_PRIV_TOKEN;
-// const merhantOwner = process.env.MID_MERCHANT;
-// const merchantStore = process.env.MID_PUFF_PALACE;
-// const sandboxUrl = process.env.SANDBOX_URL;
-
-// const config = {
-//   headers: {
-//     Authorization: `Bearer ${cloverApiToken}`,
-//   },
-// };
-
-// // axios.get();
-
-// const options = {
-//   method: "GET",
-//   headers: {
-//     accept: "application/json",
-//     authorization: "Bearer " + cloverApiToken,
-//   },
-// };
-
-// fetch("https://sandbox.dev.clover.com/v3/merchants/mId", options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
-
-//fetch merchant data
 
 async function getMerchantData() {
   const merchantId = process.env.MID_PUFF_PALACE;
@@ -46,7 +15,10 @@ async function getMerchantData() {
     });
     return response.data;
   } catch (error) {
-    console.error("Failed to fetch merchant data in clover_api.js:", error);
+    console.error(
+      "Error Details:",
+      error.response ? error.response.data : error.message
+    );
     throw error;
   }
 }
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const getMerchantData = require("./clover_api");
+const { getMerchantData } = require("./clover_api");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/api/merchant", async (req, res) => {
+    console.log("Route /api/merchant accessed");
   try {
     //fetch data from api
     const data = await getMerchantData();
-    res.join(data);
+    res.json(data);
   } catch (error) {
+    console.error("Failed to fetch merchant data:", error.message);
     res.status(500).json({ error: "Failed to fetch merchant data" });
   }
 });
